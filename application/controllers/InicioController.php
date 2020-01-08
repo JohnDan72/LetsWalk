a3b88889b0c7698039e5fe5a749106de7678db74
Jesus Martin <jesus.martinsamano@gmail.com>
2020-01-08 16:08:47 -0600

    Se movio la carpeta private un nivel arriba

--- application/controllers/InicioController.php
+++ application/controllers/InicioController.php
@@ -5,13 +5,13 @@ class InicioController extends CI_Controller {
 		function __construct(){
 			parent::__construct();
 		}
-	
+
 		function index(){
 			$dato['string'] = "Let's Walk | Inicio";
 
-			$this->load->view('Templates/header',$dato);
+			//$this->load->view('Templates/header',$dato);
 			$this->load->view('Inicio/InicioView');
-			$this->load->view('Templates/footer');
+			//$this->load->view('Templates/footer');
 		}
 }
 
