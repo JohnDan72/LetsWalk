0025756dd4f98987aa43981330d8a65fbea37961
Juan Dan <mr._dany@hotmail.com>
2020-01-08 15:12:20 -0600

    prueba commit 2

--- application/views/Inicio/InicioView.php
+++ application/views/Inicio/InicioView.php
@@ -1,3 +1,5 @@
+<!--COMENTARIO DE PRUEBA PARA GIT-->
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="<?= base_url() ?>Estilos/css/animate.css">
