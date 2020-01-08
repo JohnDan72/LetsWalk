562a2358cf86d3622c22f6a81e70efc19b0b9c67
Juan Dan <mr._dany@hotmail.com>
2020-01-08 16:05:40 -0600

    cambio en view de master

--- application/views/Inicio/InicioView.php
+++ application/views/Inicio/InicioView.php
@@ -1,3 +1,4 @@
+<!--Cambio de master-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="<?= base_url() ?>Estilos/css/animate.css">
