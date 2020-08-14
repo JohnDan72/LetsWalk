df40fae5deefffd37d7e23a759e4aa56f44c6bb6
Jesus Martin <jesus.martinsamano@gmail.com>
2020-08-13 22:51:48 -0500

    ggg

--- application/views/login/forgotView.php
+++ application/views/login/forgotView.php
@@ -71,6 +71,25 @@
         </div>
     </div>
 
+
+    <div class="container mt-5">
+
+        <div class=" d-flex justify-content-center">
+            <img src="<?= base_url() ?>private/media/icons/enviar.svg" alt="" width="200px" class="mt-5">
+        </div>
+
+        <h2 class="display-4 text-center mt-4">¡El correo ya va en camino!</h2>
+        <h4 class="text-center mt-3">Se ha iniciado el proceso de reestablecimiento de contraseña</h4>
+        <p class="text-center mt-5">Si la información que ingresaste coincide con lo que sabemos, deberas recibir un mail a la brevedad.
+        </p>
+
+        <p class="text-center mb-5"> <a href="<?= base_url() ?>loginController">Inicia sesión</a></p>
+
+    </div>
+
+
+
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?= base_url() ?>private/js/bootstrap/bootstrap.min.js"></script>
